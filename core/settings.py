6a574471f0e5494d4aae32bbe5ceba5d7ba73264
Ludovic <villeg300@gmail.com>
2025-09-05 03:39:26 +0000

    static

--- core/settings.py
+++ core/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'store.middleware.CartMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'core.urls'
 
